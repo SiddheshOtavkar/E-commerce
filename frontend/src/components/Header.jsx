9fd058a07a2ef15b0be96f5099c9bed3fa87a254
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-03-07 09:40:45 +0530

    Header and cdn links

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { NavLink, Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header-top-strip py-3'>
+      <div className="container-xxl">
+        <div className="row">
+          <div className='col-6'>
+            <p>Free Shipping Over Rs2000 & Free Returns</p>
+          </div>
+          <div className='col-6'>
+            <p>Hotline: <a href="tel:+91 8879025972">+91 8879025972</a> </p>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
